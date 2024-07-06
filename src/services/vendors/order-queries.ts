a4beebfd0b949ccbc6cb712147d3acb98c1932c9
CodeNoJutsu <bellokhalid74@gmail.com>
2024-07-06 14:24:25 +0100

    debugged paid orders api for staffs

--- src/services/vendors/order-queries.ts
+++ src/services/vendors/order-queries.ts
@@ -3,9 +3,9 @@ import pool from "../../modules/connectdb";
 // query paid orders sorted by payment data
 const queryPaidOrders = (vendorId: number, pagin: number, limit: number) => {
     return new Promise((resolve, reject) => {
-        const query = 'SELECT * FROM orders WHERE vendor_id = ?  AND status = paid ORDER BY payment_date DESC LIMIT ? OFFSET ?';
+        const query = 'SELECT * FROM orders WHERE vendor_id = ?  AND status = ? ORDER BY payment_date DESC LIMIT ? OFFSET ?';
 
-        pool.query(query, [vendorId, limit, pagin], (err, result) => {
+        pool.query(query, [vendorId, 'paid', limit, pagin], (err, result) => {
             if (err) return reject(err);
 
             resolve(result);
